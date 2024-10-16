f926b0e630c03c7c564b82f6d2603e50c87ce507
Elianmrey <elian060893@gmail.com>
2024-10-16 00:52:35 -0300

    Otimização de cards para renderização de buscas com array

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import NavBar from '../../components/navBar/NavBar.jsx';
-import Card from '../../components/cardFilms/cardFilms.jsx';
-import CardTvShows from '../../components/cardTvShows/cardTvShows.jsx';
+import Card from '../../components/cardFilms/Card.jsx';
 import StyleLayout from './layout.module.css';
 import SearchBar from '../../components/Search/SearchBar.jsx';
 import { useEffect, useState } from 'react';
@@ -39,6 +38,18 @@ export default function Home() {
 
         fetchData();
     }, []);
+
+
+
+    const [movieData, setMovieData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const fetchedImages = await GetCardInfo('https://api.themoviedb.org/3/discover/movie?language=pt-BR');
+            setMovieData(fetchedImages);
+        }
+        fetchData();
+    }, []);
             
         
     return (
@@ -52,12 +63,12 @@ export default function Home() {
 
                 <div className={StyleLayout.filmsContainer}>
                     <h2 className={StyleLayout.titleFilms}>Filmes</h2>
-                    <Card searchTerm={searchResults} />
+                    <Card info={movieData} cardType="movies" />
                 </div>
 
                 <div className={StyleLayout.showsContainer}>
                     <h2 className={StyleLayout.titleFilms}>Series</h2>
-                    <CardTvShows searchTerm={searchResults} info={tvData}/>
+                    <Card info={tvData} cardType="tvShows" />
                 </div>
             </div>
         </div>
