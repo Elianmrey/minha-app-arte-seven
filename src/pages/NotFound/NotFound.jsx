ffbc3d0980e74620180648d3a6846afce97ffc37
Elianmrey <elian060893@gmail.com>
2024-10-21 10:14:28 -0300

    Ajustes visuais da NotFound Page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,11 @@
-
+import StyleNotFound from "./NotFoundStyle.module.css"
+import error404 from '../../components/img/Erro404-dark.png'
 export default function NotFound() {
     return (
-        <div className="container">
-
-            <span>Eu sou um Not Found Page</span>
-
+        <div className={StyleNotFound.container}>
+           
+            <img className={StyleNotFound.image}  src={error404} alt="Erro 404" title="Não encontramos o que busca por aqui :(" />
+            <img className={StyleNotFound.imageFloat} src={error404} alt="Erro 404" title="Não encontramos o que busca por aqui :(" />
         </div>
     )
 }
