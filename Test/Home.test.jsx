a7755ab824c120101d0d2818d67a803ae521fa98
Elianmrey <elian060893@gmail.com>
2024-12-13 21:26:58 -0300

    hometest----

---
+++ Test/Home.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Home from '../src/pages/Home/Home.jsx';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'; 
+
+import {  expect } from 'jest'
+
+
+
+
+describe('Renderiza o componente Home corretamente', () => {
+ 
+   it('renders Home corretamente', () => {
+    render(<Home />);
+    expect(screen.getByText(/Filmes/i)).toBeInTheDocument();
+    expect(screen.getByText(/Buscar/i)).toBeInTheDocument();
+    expect(screen.getByText(/Alugar/i)).toBeInTheDocument();
+  });
+});
